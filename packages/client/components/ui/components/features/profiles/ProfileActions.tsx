08f276c70861024505b0c5c7e967de0e7c2ef064
Timothy Heironimus <timothyheironimus6+github@gmail.com>
2026-08-20 11:23:32 -0500

    Fix profile message navigation

--- packages/client/components/ui/components/features/profiles/ProfileActions.tsx
+++ packages/client/components/ui/components/features/profiles/ProfileActions.tsx
@@ -30,7 +30,7 @@ export function ProfileActions(props: {
    * Open direct message channel
    */
   function openDm() {
-    props.user.openDM().then((channel) => navigate(channel.url));
+    props.user.openDM().then((channel) => navigate(channel.path));
   }
 
   /**
